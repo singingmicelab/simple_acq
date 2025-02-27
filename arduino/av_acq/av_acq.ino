e272f8cc27f7830bb875a34d512e368b94b9d03d
Mike Zheng <xzheng902@gmail.com>
2025-02-27 09:55:51 -0500

    edits

--- arduino/av_acq/av_acq.ino
+++ arduino/av_acq/av_acq.ino
@@ -1,13 +1,13 @@
 int pinState = 0;
 String MyString = "0";
 int camPin = 11;
-int micPin = 12;
+// int micPin = 12;
 int MyDelay = 9926;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(camPin, OUTPUT);
-  pinMode(micPin, OUTPUT);
+  // pinMode(micPin, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -18,7 +18,7 @@ void loop() {
     MyString = Serial.readString();
     pinState = MyString.toInt();
   }
-  digitalWrite(micPin,pinState);
+  // digitalWrite(micPin,pinState);
   digitalWrite(camPin,pinState);
   delayMicroseconds(MyDelay);
   digitalWrite(camPin,LOW);
